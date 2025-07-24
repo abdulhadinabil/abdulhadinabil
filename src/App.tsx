8a0400ae8407c48a13408aa78cda590d5029fbe9
Abdul Hadi Nabil <113552328+abdulhadinabil@users.noreply.github.com>
2025-07-24 23:09:03 +0600

    Updated App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { AuthProvider } from './contexts/AuthContext';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import HomePage from './pages/HomePage';
+import AboutPage from './pages/AboutPage';
+import ProjectsPage from './pages/ProjectsPage';
+import PhotographyPage from './pages/PhotographyPage';
+import BlogPage from './pages/BlogPage';
+import ContactPage from './pages/ContactPage';
+import BlogPostPage from './pages/BlogPostPage';
+import PhotoDetailPage from './pages/PhotoDetailPage';
+
+function App() {
+  return (
+    <AuthProvider>
+      <Router>
+        <div className="flex flex-col min-h-screen bg-gray-50">
+          <Header />
+          <main className="flex-grow">
+            <Routes>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/about" element={<AboutPage />} />
+              <Route path="/projects" element={<ProjectsPage />} />
+              <Route path="/photography" element={<PhotographyPage />} />
+              <Route path="/photography/:id" element={<PhotoDetailPage />} />
+              <Route path="/blog" element={<BlogPage />} />
+              <Route path="/blog/:id" element={<BlogPostPage />} />
+              <Route path="/contact" element={<ContactPage />} />
+            </Routes>
+          </main>
+          <Footer />
+        </div>
+      </Router>
+    </AuthProvider>
+  );
+}
+
+export default App;
